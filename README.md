e6300b19364cdf13db0bab59328e271d1e769e70
jopemachine <jopemachine@naver.com>
2021-07-05 00:37:16 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <img src="https://github.com/darrikonn/alfred-nym/blob/master/icon.png?raw=true" width=80 alt="Icon"/>
+  <h2 align="center">arvis-nym</h2>
+</p>
+
+
+## Installation
+
+### via npm
+```bash
+npm install -g arvis-nym
+```
+
+## Getting started
+Run `nym` or `snym` to get **synonyms**. Pass the `-i` flag or run `anym` to get **antonyms**:
+
+- Run `nym happy` to get synonyms of *happy*.
+
+- Run `snym happy` to get synonyms of *happy*.
+
+- Run `nym happy -i` to get antonyms of *happy*.
+
+- Run `anym happy` to get antonyms of *happy*.
---
+++ index.js
@@ -0,0 +1,20 @@
+const arvish = require("arvish");
+
+const input = arvish.input.replace(/ -i$/g, "");
+const nym = arvish.input === input ? "synonyms" : "antonyms";
+
+const { data } = await arvish.fetch(
+  `https://tuna.thesaurus.com/pageData/${input}`
+);
+const definitions = data ? data.definitionData.definitions[0][nym] : [];
+if (definitions.length === 0) {
+  return arvish.log(`No ${nym} found for "${input}"`);
+}
+
+const items = definitions.map(item => ({
+  title: item.term,
+  subtitle: `Similarity: ${item.similarity}`,
+  arg: item.term
+}));
+
+arvish.output(items);
